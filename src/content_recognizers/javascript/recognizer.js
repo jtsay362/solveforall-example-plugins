067a123c4109b7b4c8035b73011c5b6f66750df0
jtsay362 <jctsay@aim.com>
2014-03-02 19:40:42 -0800

    Add Javascript recognizer

---
+++ src/content_recognizers/javascript/recognizer.js
@@ -0,0 +1,20 @@
+function recognize(s, context)
+{    
+  try
+  {
+    var result = eval('function () {' + s + '}');
+    
+    return {
+      'com.solveforall.recognition.programming.Javascript' : [
+        {
+          matchedText: s,
+          recognitionLevel : 1.0,
+        }
+      ]
+    };            
+  }
+  catch (e)
+  {
+    return null;        
+  }    
+}
