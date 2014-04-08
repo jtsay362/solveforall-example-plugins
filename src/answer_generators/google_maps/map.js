3ea8a76c5e5f19efaaa592ceb5768669f2377a9d
jtsay362 <jctsay@aim.com>
2014-04-07 22:56:09 -0700

    Externalize Google Maps Javascript so it can be displayed in Chrome extension

---
+++ src/answer_generators/google_maps/map.js
@@ -0,0 +1,85 @@
+$(function () {
+  var map;
+  var geocoder;
+  var targetLatLng;
+  var targetMarker;
+  var infoWindow;
+
+  function updateLocation(position) {
+    var start = new google.maps.LatLng(parseFloat(position.coords.latitude),
+      parseFloat(position.coords.longitude));
+
+    map.setCenter(start);
+  }
+
+  var zoom = 15;
+
+  try {
+    zoom = parseInt($('#data').attr('data-zoom') || '15');
+  } catch (e) {
+    console.error('Can\'t parse zoom: "' + settings.zoom + '"');
+  }
+
+  var mapType = $('#data').attr('data-map-type');
+
+  if (mapType.length == 0) {
+    mapType = 'roadmap';
+  }
+
+  var mapTypeId = google.maps.MapTypeId[mapType.toUpperCase()] ||
+    google.maps.MapTypeId.ROADMAP;
+
+  var myOptions = {
+    zoom: zoom,
+    //center: targetLatLng,
+    mapTypeId: mapTypeId
+  };
+
+  map = new google.maps.Map($('#map_canvas').get(0), myOptions);
+
+  infoWindow = new google.maps.InfoWindow();
+
+  geocoder = new google.maps.Geocoder();
+
+  var address = $('#data').attr('data-address');
+
+  geocoder.geocode({
+    address: address
+  }, function (results, status) {
+    if ((status == google.maps.GeocoderStatus.OK) && (results.length > 0) &&
+        results[0].geometry && results[0].geometry.location) {
+      targetLatLng = results[0].geometry.location;
+      map.setCenter(targetLatLng);
+
+      targetMarker = new google.maps.Marker({
+        position: targetLatLng,
+        map: map,
+        title: address,
+        animation: google.maps.Animation.DROP
+      });
+
+      infoWindow.setContent(address);
+      infoWindow.open(map, targetMarker);
+
+      google.maps.event.addListener(targetMarker, 'click', function() {
+         infoWindow.setContent(address);
+         infoWindow.open(map, targetMarker);
+      });
+
+      if (($('#data').attr('data-tilt') === 'false') &&
+          ((mapTypeId === google.maps.MapTypeId.SATELLITE) ||
+           (mapTypeId === google.maps.MapTypeId.HYBRID))) {
+        map.setTilt(0);
+      }
+    } else {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(updateLocation);
+      }
+    }
+  });
+
+/*
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(updateLocation);
+    } */
+});
