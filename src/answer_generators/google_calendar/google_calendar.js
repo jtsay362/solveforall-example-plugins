09635606cb09611e9287480c17b81d7a5ba83972
jtsay362 <jctsay@aim.com>
2014-04-10 00:48:30 -0700

    Add Google Calendar Answer Generator

---
+++ src/answer_generators/google_calendar/google_calendar.js
@@ -0,0 +1,37 @@
+function pad(num, length, def) {
+  return _('' + (num || def)).pad(length, '0');
+}
+
+function generateResults(recognitionResults, q, context) {
+  'use strict';
+
+  var recognitionResult = recognitionResults['com.solveforall.recognition.date.DateRange'];
+
+  if (!recognitionResult || (recognitionResult.length == 0)) {
+    console.warn("No recognition result found");
+    return [];
+  }
+
+  var dateRange = recognitionResult[0];
+  var start = dateRange.start;
+
+  if (!start) {
+    console.warn("No start date found");
+    return [];
+  }
+
+  var now = new Date();
+  var paddedMonth = pad(start.month, 2, now.getMonth() + 1);
+  var paddedDay = pad(start.dayOfMonth, 2, now.getDate());
+  var year = '' + (start.year || now.getFullYear());
+
+  return [{
+    label: 'Calendar',
+    iconUrl: 'http://calendar.google.com/googlecalendar/images/favicon_v2010_8.ico',
+    summaryHtml: 'Google Calendar',
+    uri: 'https://www.google.com/calendar/render?tab=mc&date=' + year +
+      paddedMonth + paddedDay,
+    embeddable: false,
+    relevance: dateRange.recognitionLevel
+  }];
+}
