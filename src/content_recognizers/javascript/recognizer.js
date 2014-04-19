726a0dc3a85407a8bc8ab6be9ff1faeb2cae0eba
jtsay362 <jctsay@aim.com>
2014-04-18 21:48:28 -0700

    If the query string is JSON (including number and boolean literals), have the recognition level for Javascript be 0.

--- src/content_recognizers/javascript/recognizer.js
+++ src/content_recognizers/javascript/recognizer.js
@@ -1,12 +1,23 @@
+/*jslint continue: true, devel: true, evil: true, indent: 2, nomen: true, plusplus: true, regexp: true, rhino: true, sloppy: true, sub: true, unparam: true, vars: true, white: true */
+/*global _, HostAdapter, hostAdapter */
+
 function recognize(s, context) {    
   try {
     var result = eval('function () {' + s + '}');
+    
+    var isJson = false;
+    try {
+      JSON.parse(s); 
+      isJson = true;
+    } catch (je) {
+      // Not JSON 
+    }
     
     return {
       'com.solveforall.recognition.programming.Javascript' : [
         {
           matchedText: s,
-          recognitionLevel : 1.0,
+          recognitionLevel: (isJson ? 0.0 : 1.0)
         }
       ]
     };            
