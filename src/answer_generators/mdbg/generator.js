40d0ad7a897cff8ff1aa60a22f74ee120e9f4bc6
jtsay362 <jctsay@aim.com>
2014-04-18 18:47:05 -0700

    Add summaryHtml and embeddable: true flag to MDBG Answer Generator

--- src/answer_generators/mdbg/generator.js
+++ src/answer_generators/mdbg/generator.js
@@ -1,21 +1,20 @@
-function generateResults
-(
-  recognitionResults,
-  q,
-  context
-)
-{
+/*jslint continue: true, devel: true, evil: true, indent: 2, plusplus: true, rhino: true, unparam: true, vars: true, white: true */
+function generateResults(recognitionResults, q, context) {
+  'use strict';
+  
   var resultList = recognitionResults['com.solveforall.recognition.lang.chinese.Chinese'];
 
-  if (!resultList)
-  {
+  if (!resultList) {
     return null;
   }
   
+  var word = resultList[0].traditional;
   return [{
     label: 'MDBG',
-    uri: 'http://www.mdbg.net/chindict/mobile.php?mwddw=' + resultList[0].traditional + '&handler=DecomposeWord',
-    iconUrl: 'http://www.mdbg.net/favicon.ico'
+    iconUrl: 'http://www.mdbg.net/favicon.ico',
+    uri: 'http://www.mdbg.net/chindict/mobile.php?mwddw=' + encodeURIComponent(word) + '&handler=DecomposeWord',
+    embeddable: true,    
+    summaryHtml: 'Lookup ' + _(word).escapeHTML() + ' on MDBG'
   }];  
 }
 
