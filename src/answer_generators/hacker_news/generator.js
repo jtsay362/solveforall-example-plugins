b4a160b4354e13e6df680541b2eedcd92fa47307
jtsay362 <jctsay@aim.com>
2014-04-18 18:39:06 -0700

    Update Hacker News generator to use new WebRequest API

--- src/answer_generators/hacker_news/generator.js
+++ src/answer_generators/hacker_news/generator.js
@@ -1,19 +1,26 @@
-function handleResponse(response) {
+/*jslint continue: true, devel: true, evil: true, indent: 2, plusplus: true, rhino: true, unparam: true, vars: true, white: true */
+function handleResponse(responseText, response) {
   'use strict';
-
+  
+  console.log('Got response');
+  
   if (response.statusCode !== 200) {
+    console.error('Got bad status code ' + response.statusCode);
     return null;
   }
 
-  var resultObject = JSON.parse(response.body);
+  var resultObject = JSON.parse(responseText);
 
+  console.log('Got ' + resultObject.hits.length + ' hits');
+  
   return _(resultObject.hits).map(function (hit) {
     return {
-      label : hit.title,
+      label : hit.title || '',
+      iconUrl: 'https://news.ycombinator.com/favicon.ico',
       uri : hit.url,
       embeddable: false,
-      summaryHtml: _(story_text).escapeHTML(),
-      relevance: 0.4 * (hit.points || 0) * 0.01
+      summaryHtml: _(hit.story_text || '').escapeHTML(),
+      relevance: 0.4 * (1.0 - Math.pow(2.0, -Math.max((hit.points || 0), 1) * 0.01))
     };
   });
 }
@@ -21,15 +28,19 @@ function handleResponse(response) {
 function generateResults(recognitionResults, q, context) {
   'use strict';
 
+  if (context.isSuggestionQuery) {
+    return [];
+  }
+  
   var url = 'https://hn.algolia.com/api/v1/search';
 
-  var request = HostAdapter.makeWebRequest(url, {
+  var request = hostAdapter.makeWebRequest(url, {
     data: {
       query: q
     }
   });
   
-  request.execute('handleResponse');
+  request.send('handleResponse');
 
   return HostAdapter.SUSPEND;
 }
