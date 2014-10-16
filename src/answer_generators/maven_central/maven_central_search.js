e5baa1ef485be6a7a1ef37bf15816ba7ac2b0ce4
Jeff Tsay <jctsay@aim.com>
2014-10-15 22:37:27 -0700

    Change icon URL for Maven Central to use HTTPS

--- src/answer_generators/maven_central/maven_central_search.js
+++ src/answer_generators/maven_central/maven_central_search.js
@@ -4,7 +4,7 @@ function generateResults(recognitionResults, q, context) {
   'use strict';
 
   var LABEL = "Maven Central";
-  var ICON_URL = "https:/search.maven.org/favicon.ico";
+  var ICON_URL = "https://search.maven.org/favicon.ico";
   var URL_PREFIX = "https://search.maven.org/#search%7Cga%7C1%7C";
   var EMBEDDABLE = false; // Page does some Ajax that doesn't work
 
