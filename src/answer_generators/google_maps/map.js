dcb1d98ce6f9acda5dc643f68b56c08ed49b17f4
Jeff Tsay <jctsay@aim.com>
2015-04-18 21:03:17 -0700

    Minor formatting / linting changes

--- src/answer_generators/google_maps/map.js
+++ src/answer_generators/google_maps/map.js
@@ -1,9 +1,9 @@
-/*jslint continue: true, devel: true, evil: true, indent: 2, nomen: true, plusplus: true, regexp: true, rhino: true, sloppy: true, sub: true, unparam: true, vars: true, white: true */
+/*jslint browser: true, continue: true, devel: true, evil: true, indent: 2, nomen: true, plusplus: true, regexp: true, sloppy: true, sub: true, unparam: true, vars: true, white: true */
 /*global $, _, google */
 
 $(function () {
   'use strict';
-  
+
   var zoom = 15;
 
   var zoomString = $('#data').attr('data-zoom');
@@ -31,15 +31,15 @@ $(function () {
   var map = new google.maps.Map($('#map_canvas').get(0), myOptions);
 
   var infoWindow = new google.maps.InfoWindow();
-    
+
   function updateLocation(position) {
     var start = new google.maps.LatLng(parseFloat(position.coords.latitude),
       parseFloat(position.coords.longitude));
 
     map.setCenter(start);
   }
-  
-  function handleLocation(targetLatLng, label) {    
+
+  function handleLocation(targetLatLng, label) {
     map.setCenter(targetLatLng);
 
     var targetMarker = new google.maps.Marker({
@@ -61,19 +61,19 @@ $(function () {
         ((mapTypeId === google.maps.MapTypeId.SATELLITE) ||
          (mapTypeId === google.maps.MapTypeId.HYBRID))) {
       map.setTilt(0);
-    }            
+    }
   }
-    
+
   var address = $('#data').attr('data-address');
   var label = $('#data').attr('data-label');
 
-  if (address && (address.length > 0)) {  
+  if (address && (address.length > 0)) {
     if (!label) {
       label = address;
     }
-    
+
     var geocoder = new google.maps.Geocoder();
-    
+
     geocoder.geocode({
       address: address
     }, function (results, status) {
@@ -89,12 +89,12 @@ $(function () {
   } else {
     var latitude = parseFloat($('#data').attr('data-latitude'));
     var longitude = parseFloat($('#data').attr('data-longitude'));
-    
+
     if (!label) {
-      label = latitude.toFixed(5) + ", " + longitude.toFixed(5);       
+      label = latitude.toFixed(5) + ", " + longitude.toFixed(5);
     }
-    
-    handleLocation(new google.maps.LatLng(latitude, longitude), label);                      
+
+    handleLocation(new google.maps.LatLng(latitude, longitude), label);
   }
 
 /*
